3ee49b5dfbad8d711a4f1c19418d6940b01cb974
Sandrine <s.fiol@proxiad.com>
2014-10-08 12:24:12 +0200

    Vue + launcher

--- src/org/imie/Vue.java
+++ src/org/imie/Vue.java
@@ -1,7 +1,10 @@
 package org.imie;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,15 +25,36 @@ public class Vue {
 			public void run () {
 				
 				JFrame frame = new JFrame("Jeu du Morpion");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setPreferredSize(new Dimension(500,500));
 				frame.getContentPane().setLayout(new BorderLayout());
 				
+				// partie haute : joueurs
 				JPanel panelHaut = new JPanel();
-				JLabel labelJoueur1 = new JLabel();
-				labelJoueur1.setPreferredSize(new Dimension (50,20));
-				JLabel labelJoueur2 = new JLabel();
-				labelJoueur2.setPreferredSize(new Dimension (50,20));
-				panelHaut.add(comp)
+				JLabel labelJoueur1 = new JLabel("Joueur 1");
+				labelJoueur1.setPreferredSize(new Dimension (100,20));
+				JLabel labelJoueur2 = new JLabel("Joueur 2");
+				labelJoueur2.setPreferredSize(new Dimension (100,20));
+				panelHaut.add(labelJoueur1);
+				panelHaut.add(labelJoueur2);
+				frame.add(panelHaut, BorderLayout.NORTH);
 				
+				// partie basse : jeu et historique des parties en cours
+				JPanel panelBas = new JPanel();
+				
+				// jeu
+				JPanel panelJeu = new JPanel();
+				panelBas.add(panelJeu);
+				
+				// historique des parties
+				JPanel panelHisto = new JPanel();
+				panelHisto.setLayout(new BoxLayout(panelHisto, BoxLayout.Y_AXIS));
+				panelBas.add(panelHisto);
+				
+				frame.add(panelHaut, BorderLayout.CENTER);
+				
+				frame.pack();
+				frame.setVisible(true);
 				
 				
 				

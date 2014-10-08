b4d28f52bec49dd629a76f0be23b2a9e09988e41
valours <leflem.valentin@gmail.com>
2014-10-08 12:46:18 +0200

    merge

---
+++ src/org/imie/Launcher.java
@@ -0,0 +1,13 @@
+package org.imie;
+
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				  
+	  Vue vueMorpion = new Vue();
+	  vueMorpion.start();
+	}
+
+}
